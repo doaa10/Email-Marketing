756999abe4206f9f035a47cbeebdc1e46100a2b8
Lujaina Walid <90357532+Lujaina21@users.noreply.github.com>
2021-09-20 20:42:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 'use strict'
+var nav = document.getElementById('nav');
+     window.onscroll = function () {
+    if(window.pageYOffset > 100) { 
+      nav.classList.add("shrink");
+    }else{
+            nav.classList.remove("shrink");
+         }
+      }
